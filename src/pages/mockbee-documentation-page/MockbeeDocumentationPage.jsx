a7804fba7ae7646ccf6d99af862ccc175b47d9b7
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-08-02 17:19:31 +0530

    feat: pages init

---
+++ src/pages/mockbee-documentation-page/MockbeeDocumentationPage.jsx
@@ -0,0 +1,7 @@
+import MockbeeDocumentation from "../../components/features/mockbee-documentation/MockbeeDocumentation";
+
+const MockbeeDocumentationPage = () => {
+  return <MockbeeDocumentation />;
+};
+
+export default MockbeeDocumentationPage;
