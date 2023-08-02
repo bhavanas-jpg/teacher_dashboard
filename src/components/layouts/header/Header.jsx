28af7908373a2a8936f81b58dd4eb5f1306c4c3c
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-08-02 17:21:00 +0530

    feat: app layout init

---
+++ src/components/layouts/header/Header.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../navbar/Navbar';
+import './Header.css';
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <header className="header">
+      <h1 onClick={() => navigate('/')}>Teachers' Dashboard</h1>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
